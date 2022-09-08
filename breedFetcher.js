f262f0018db10f51c3384f0cc1517d114eaea369
jehmatthews <jehmatthews30@gmail.com>
2022-09-08 13:42:27 -0700

    finding the description of a cat through a http request

---
+++ breedFetcher.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const breed = process.argv[2].slice(0, 4).toLowerCase();
+
+
+request(`https://api.thecatapi.com/v1/breeds/${breed}`, (error, response, body) => {
+  if (body === "{}") {
+    console.log('Breed is not found');
+    return;
+  }
+  console.log("Error: ", error);
+  const data = JSON.parse(body);
+  console.log(data.description);
+});
